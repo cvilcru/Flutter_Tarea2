c76a0bfaf51c461e6236ada3f6a270bb5bfb66bc
cvilcru <cvilcru430@g.educaand.es>
2024-05-28 10:52:26 +0200

    Add initial classes to start the task
    Added game.dart.
    Invoked GamePage view in app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tarea2/app/view/game.dart';
+import 'package:flutter_tarea2/app/view/splash.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tarea 2',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: GamePage(),
+    );
+    // This is the theme of your application.
+  }
+}
